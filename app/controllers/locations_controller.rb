861f94f46bee8c725926f460ac2463846d9d8c85
Ryan Yep <ryan.yep@gmail.com>
2016-03-15 16:58:47 -0500

    Add location controller

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,36 @@
+class LocationsController < ApplicationController
+  before_action :set_location, only: [:show, :destroy]
+
+  def index
+    @locations = Location.all
+  end
+
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+    if @location.save
+      redirect_to locations_path
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    if @location.destroy
+      render text: "Successfully deleted a location!", status: 200
+  end
+
+  private
+
+  def set_location
+    @location = Location.find(params[:id])
+  end
+
+  def location_params
+    params.require(:location).permit(:address1, :city, :state, :zipcode, :latitude, :longitude, :user_id)
+  end
+
+end
