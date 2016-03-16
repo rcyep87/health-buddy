7f44c612a6168236c9aa3a770fa031000ad5d67e
Ryan Yep <ryan.yep@gmail.com>
2016-03-16 15:39:53 -0500

    Create methods on user model to only display most recent latitude and longitude for first user for testing purposes

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,12 @@ class User < ActiveRecord::Base
   has_many :sleeps
   has_many :locations
 
+  def most_recent_lat #displays most recent latitude for a user's location
+    self.first.locations.last.latitude
+  end
+
+  def most_recent_long #displays most recent longitude for a user's location
+    self.first.locations.last.longitude
+  end
 
 end
