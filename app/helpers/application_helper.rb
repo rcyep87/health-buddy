c51bc2da023b59bf1507a34ce884704f84a6ab93
Ryan Yep <ryan.yep@gmail.com>
2016-04-07 17:23:08 -0500

    Fixed css and javascript to change depending on time of day

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def time_of_day
+    case Time.now.hour
+    when 0..5, 22..24 then "night"
+    when 5..11        then "morning"
+    when 11..16       then "day"
+    when 16..22       then "evening"
+    else
+      "day"
+    end
+  end
+
+  def time_of_day_random
+    %w(morning day night evening).sample
+  end
+
 end
